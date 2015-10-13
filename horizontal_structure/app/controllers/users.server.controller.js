ad54cd36877614a660076dffa6568783f4fe8496
lakatos.tamas <lakatos.tamas@virgo.hu>
2015-10-13 09:40:30 +0200

    facebook login

--- horizontal_structure/app/controllers/users.server.controller.js
+++ horizontal_structure/app/controllers/users.server.controller.js
@@ -55,7 +55,7 @@ exports.signup = function(req, res, next) {
     user.save(function(err) {
       if (err) {
         var message = getErrorMessage(err);
-        
+
         req.flash('error', message);
         return res.redirect('/signup');
       }
@@ -72,6 +72,40 @@ exports.signup = function(req, res, next) {
 exports.signout = function(req, res) {
   req.logout();
   res.redirect('/');
+}
+
+exports.saveOAuthUserProfile = function(req, profile, done) {
+  User.findOne({
+    provider: profile.provider,
+    providerId: profile.providerId
+  }, function(err, user) {
+    if (err) {
+      return done(err);
+    } else {
+      if (!user) {
+        var possibleUserName = profile.username || ((profile.email) ? profile.email.split('@')[0] : '');
+
+        User.findUniqueUsername(possibleUserName, null, function(availableUserNAme) {
+          profile.username = availableUserNAme;
+
+          user = new User(profile);
+
+          user.save(function(err) {
+            if (err) {
+              var message = _this.getErrorMessage(err);
+
+              req.flash('error', message);
+              return res.redirect('/signup');
+            }
+
+            return done(err, user);
+          });
+        });
+      } else {
+        return done(err, user);
+      }
+    }
+  });
 }
 /*
 exports.create = function(req, res, next) {
--- horizontal_structure/config/env/development.js
+++ horizontal_structure/config/env/development.js
@@ -1,4 +1,9 @@
-module.exports =  {
-	db: 'mongodb://localhost/mean-book',
-	sessionSecret: 'developmentSessionSecret'
+module.exports = {
+  db: 'mongodb://localhost/mean-book',
+  sessionSecret: 'developmentSessionSecret',
+  facebook: {
+    clientID: '718152964984164',
+    clientSecret: '6812bbb3670fc921bdcfa74a24a9ae03',
+    callbackURL: 'http://localhost:3001/oauth/facebook/callback'
+  }
 };
